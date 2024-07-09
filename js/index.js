537f6255fe001ac5de3fc554a6890d8837e161fc
mgambin47 <155911919+mgambin47@users.noreply.github.com>
2024-07-08 18:26:01 -0700

    hoping this works

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("woo");
+
+$("#DRB").on("click", function(){
+    console.log("Made ya look! You clicked on download resume");
+});
+
+// $(".skill").on( "mouseenter", handlerIn ).on( "mouseleave", handlerOut );{
+//     console.log("hovering yep");
+// });
+
+$(".skill").on("click", function(){
+    console.log("hovering yep");
+});
